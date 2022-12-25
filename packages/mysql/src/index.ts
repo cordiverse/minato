11702ccc911a4dc7cae0f55a868a8730bd7c5bed
Seidko <64234553+Seidko@users.noreply.github.com>
2022-12-25 12:25:10 +0800

    feat: drop api support in mysql

--- packages/mysql/src/index.ts
+++ packages/mysql/src/index.ts
@@ -326,6 +326,10 @@ class MySQLDriver extends Driver {
     return this.queue(sql, values)
   }
 
+  async drop(table: string) {
+    this.query(`DROP TABLE ${escapeId(table)}`)
+  }
+
   async drop() {
     const data = await this._select('information_schema.tables', ['TABLE_NAME'], 'TABLE_SCHEMA = ?', [this.config.database])
     if (!data.length) return
