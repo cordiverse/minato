9d94c78b7338006debd77ef59a5361735ecdef47
iFrank <frank@ifrank.cn>
2023-04-16 00:40:30 +0800

    fix(sqlite): fix `stats()` no returns (#22)

--- packages/sqlite/src/index.ts
+++ packages/sqlite/src/index.ts
@@ -284,7 +284,14 @@ export class SQLiteDriver extends Driver {
   async stats() {
     const data = this.db.export()
     this.init(data)
-    return { size: data.byteLength }
+    const stats: Driver.Stats = { size: data.byteLength, tables: {} }
+    const tableNames: Array<{ name: string }> = this.#all('SELECT name FROM sqlite_master WHERE type="table" ORDER BY name;')
+    const dbstats: Array<{ name: string, size: number }> = this.#all('SELECT name, pgsize as size FROM "dbstat" WHERE aggregate=TRUE;')
+    tableNames.forEach(tbl => {
+      stats.tables[tbl.name] = this.#get(`SELECT COUNT(*) as count FROM ${escapeId(tbl.name)};`)
+      stats.tables[tbl.name].size = dbstats.find(o => o.name === tbl.name)!.size
+    })
+    return stats
   }
 
   async remove(sel: Selection.Mutable) {
