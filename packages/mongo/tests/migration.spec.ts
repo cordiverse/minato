33e6555a3ae49ad50359ac052111949f71b78b80
Hieuzest <girkirin@hotmail.com>
2023-08-22 02:43:36 +0800

    test(mongo): fix coverage

--- packages/mongo/tests/migration.spec.ts
+++ packages/mongo/tests/migration.spec.ts
@@ -25,8 +25,8 @@ interface Tables {
 describe('@minatojs/driver-mongo/migrate-virtualKey', () => {
   const database: Database<Tables> = new Database()
 
-  const initialize = async (optimizeIndex: boolean) => {
-    logger.level = 3
+  const resetConfig = async (optimizeIndex: boolean) => {
+    await database.stopAll()
     await database.connect('mongo', {
       host: 'localhost',
       port: 27017,
@@ -35,22 +35,25 @@ describe('@minatojs/driver-mongo/migrate-virtualKey', () => {
     })
   }
 
-  const finalize = async () => {
-    await database.stopAll()
-    logger.level = 2
-  }
+  beforeEach(async () => {
+    logger.level = 3
+    await database.connect('mongo', {
+      host: 'localhost',
+      port: 27017,
+      database: 'test',
+      optimizeIndex: false,
+    })
+  })
 
-  after(async () => {
+  afterEach(async () => {
     await database.dropAll()
     await database.stopAll()
     logger.level = 2
   })
 
   it('reset optimizeIndex', async () => {
-    await initialize(false)
-
     database.extend('temp1', {
-      id: 'primary',
+      id: 'unsigned',
       text: 'string',
       value: 'integer',
       bool: 'boolean',
@@ -74,22 +77,51 @@ describe('@minatojs/driver-mongo/migrate-virtualKey', () => {
     table.push(await database.create('temp1', { text: 'awesome baz' }))
     await expect(database.get('temp1', {})).to.eventually.have.shape(table)
 
-    await finalize()
-    await initialize(true)
+    await resetConfig(true)
     await expect(database.get('temp1', {})).to.eventually.have.shape(table)
 
-    await finalize()
-    await initialize(false)
-    await expect(database.get('temp1', {})).to.eventually.have.shape(table)
-
-    await (Object.values(database.drivers)[0] as MongoDriver).drop('_fields')
-    await finalize()
-    await initialize(true)
+    await resetConfig(false)
     await expect(database.get('temp1', {})).to.eventually.have.shape(table)
 
     await (Object.values(database.drivers)[0] as MongoDriver).drop('_fields')
-    await finalize()
-    await initialize(false)
+    await resetConfig(true)
+    await expect(database.get('temp1', {})).to.eventually.have.shape(table)
+
+    await (Object.values(database.drivers)[0] as MongoDriver).drop('_fields')
+    await resetConfig(false)
+    await expect(database.get('temp1', {})).to.eventually.have.shape(table)
+  })
+
+  it('using primary', async () => {
+    database.extend('temp1', {
+      id: 'primary',
+      text: 'string',
+      value: 'integer',
+      bool: 'boolean',
+      list: 'list',
+      timestamp: 'timestamp',
+      date: 'date',
+      time: 'time',
+      regex: 'string',
+    })
+
+    const table: Foo[] = []
+    table.push(await database.create('temp1', {
+      text: 'awesome foo',
+      timestamp: new Date('2000-01-01'),
+      date: new Date('2020-01-01'),
+      time: new Date('2020-01-01 12:00:00'),
+    }))
+    table.push(await database.create('temp1', { text: 'awesome bar' }))
+    table.push(await database.create('temp1', { text: 'awesome baz' }))
+    await expect(database.get('temp1', {})).to.eventually.have.shape(table)
+
+    await (Object.values(database.drivers)[0] as MongoDriver).drop('_fields')
+    await resetConfig(true)
+    await expect(database.get('temp1', {})).to.eventually.have.shape(table)
+
+    await (Object.values(database.drivers)[0] as MongoDriver).drop('_fields')
+    await resetConfig(false)
     await expect(database.get('temp1', {})).to.eventually.have.shape(table)
   })
 })
