a5c03a7cc6b59f856d95395212f8d896d70215b9
Hieuzest <girkirin@hotmail.com>
2023-11-18 00:47:15 +0800

    chore: replace chai-shape

---
+++ packages/tests/src/shape.ts
@@ -0,0 +1,87 @@
+import { inspect } from 'util'
+
+function flag(obj, key, value?) {
+  var flags = obj.__flags || (obj.__flags = Object.create(null));
+  if (arguments.length === 3) {
+    flags[key] = value;
+  } else {
+    return flags[key];
+  }
+};
+
+function isSubsetOf(subset, superset, cmp, contains, ordered) {
+  if (!contains) {
+    if (subset.length !== superset.length) return false;
+    superset = superset.slice();
+  }
+
+  return subset.every(function (elem, idx) {
+    if (ordered) return cmp ? cmp(elem, superset[idx]) : elem === superset[idx];
+
+    if (!cmp) {
+      var matchIdx = superset.indexOf(elem);
+      if (matchIdx === -1) return false;
+
+      // Remove match from superset so not counted twice if duplicate in subset.
+      if (!contains) superset.splice(matchIdx, 1);
+      return true;
+    }
+
+    return superset.some(function (elem2, matchIdx) {
+      if (!cmp(elem, elem2)) return false;
+
+      // Remove match from superset so not counted twice if duplicate in subset.
+      if (!contains) superset.splice(matchIdx, 1);
+      return true;
+    });
+  });
+}
+
+export = (({ Assertion }) => {
+  function checkShape(expect, actual, path, ordered) {
+    if (actual === expect || Number.isNaN(expect) && Number.isNaN(actual)) return
+
+    function formatError(expect, actual) {
+      return `expected to have ${expect} but got ${actual} at path ${path}`
+    }
+
+    if (!expect || ['string', 'number', 'boolean', 'bigint'].includes(typeof expect)) {
+      return formatError(inspect(expect), inspect(actual))
+    }
+
+    // dates
+    if (expect instanceof Date) {
+      if (!(actual instanceof Date) || +expect !== +actual) {
+        return formatError(inspect(expect), inspect(actual))
+      }
+      return
+    }
+
+    if (actual === null) {
+      const type = Object.prototype.toString.call(expect).slice(8, -1).toLowerCase()
+      return formatError(`a ${type}`, 'null')
+    }
+
+    // array / object
+    if (!ordered && Array.isArray(expect) && Array.isArray(actual)) {
+      if (!isSubsetOf(expect, actual, (x, y) => !checkShape(x, y, `${path}/`, ordered), false, false)) {
+        return `expected same shape of members`
+      }
+      return
+    }
+
+    for (const prop in expect) {
+      if (typeof actual[prop] === 'undefined' && typeof expect[prop] !== 'undefined') {
+        return `expected "${prop}" field to be defined at path ${path}`
+      }
+      const message = checkShape(expect[prop], actual[prop], `${path}${prop}/`, ordered)
+      if (message) return message
+    }
+  }
+
+  Assertion.addMethod('shape', function (expect) {
+    var ordered = flag(this, 'ordered');
+    const message = checkShape(expect, this._obj, '/', ordered)
+    if (message) this.assert(false, message, '', expect, this._obj)
+  })
+}) as Chai.ChaiPlugin
